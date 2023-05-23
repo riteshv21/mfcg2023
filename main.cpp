531d888994f3e87ddbbd7f0af67b2ccb56be0015
Ritesh Verma <96976400+riteshv21@users.noreply.github.com>
2023-05-24 00:04:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include "vector.h"
+#include "includes/Math/vector.h"
 
 int main()
 {
     std::cout<<"Assignment One Evaluation"<<std::endl;
 
     Vector3 vec1(1,2,3);
+    Vector3 vec2(4,5,6);
     vec1.print();
+    vec1.lengthOfVector(3,4,5);
+    vec1.operator*(7);
+    vec1.operator*(vec2);
+    vec1.dotProduct(vec2);
+    vec1.crossProduct(vec2);
+    vec1.operator+(vec2);
+    vec1.operator/(5);
+    vec1.operator-(vec2);
     std::cin.get();
     return 0;
 }
